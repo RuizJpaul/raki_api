d5d912678b1cd6e81792cdac6b9ae92755c22e78
Jean Paul <jpaul.ruiz.1802@gmail.com>
2025-11-23 19:28:11 -0500

    ruta register agregada

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,9 +4,10 @@ import { registrar, login, perfil } from "../controllers/authController.js";
 
 const router = express.Router();
 
-
 import { verificarToken } from "../middleware/authMiddleware.js";
 
+router.post("/register", registrar);
+router.post("/login", login);
 router.get("/profile", verificarToken, perfil);
 
 export default router;
